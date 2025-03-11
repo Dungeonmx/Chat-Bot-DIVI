19d40533c055b9b015449e83673d72744099a60b
Benjamin <benjamin@localhost.localdomain>
2025-03-11 08:50:21 -0300

    comentarios a funciones y chroma

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import ollama
 from ollama import chat
 from ollama import ChatResponse
 import gradio as gr
@@ -7,32 +8,64 @@ from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
 from elevenlabs import VoiceSettings
 
+
+# CARGO VARIABLES DE ENTORNO (api keys)
+
 load_dotenv()
+
+# CONSTANTES 
 
 ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 
 MODEL_AI = 'deepseek-r1:1.5b'
 
 
+# FUNCIONES
+
+
 def preguntar(pregunta):
-	response: ChatResponse = chat(model=MODEL_AI, messages=[
-	  {
-	    'role': 'user',
-	    'content': pregunta,
-	  },
-	])
+	"""
+	Resive la una pregunra en forma de texto y gera una respueta
+	con la IA
+	"""
+
+	response: ChatResponse = chat(
+		model=MODEL_AI,
+
+		messages=[
+		 	{
+		    	'role': 'user',
+		    	'content': pregunta,
+		  	},
+		]
+	)
 
 	return response.message.content
 
+
+
 def to_text(audio_file):
+	"""
+	Convierte el audio de input a texto
+	"""
 	r = sr.Recognizer()
+	
 	with sr.AudioFile(audio_file) as source:
 		audio = r.listen(source)
 		transcription = r.recognize_google(audio, language="es-MX")
+	
 	return transcription
 
+
+
+
 def to_speech(texto):
+	"""
+	Convierte el texto de string a audio,
+	retornando la direccion al archivo de audio
+	"""
 	client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+	
 	audio = client.text_to_speech.convert(
 		voice_id="pNInz6obpgDQGcFmaJgB",  # Adam pre-made voice
         optimize_streaming_latency="0",
@@ -55,27 +88,59 @@ def to_speech(texto):
 
 	return save_file_path
 
+
+
+
 def answer(audio_file):
+	"""
+	Convina todas las funciones de forma coordinada
+	para resibir la direccion al archivo de audio de entrada
+	y retornar la respuesta en forma de audio por la salida
+	"""
 	question = to_text(audio_file)
 	answer = preguntar(question)
 	audio_file_path = to_speech(answer)
+	
 	return audio_file_path
+
+
+# INTERFAZ WEB
 
 web = gr.Interface(
 
 	fn=answer,
+	
 	inputs=gr.Audio(
 			label="You",
 			sources=["microphone"],
 			type="filepath"
 		),
+	
 	outputs=gr.Audio(
 			label="AI",
 		),
+	
 	title="VoiceBot",
-	# outputs=[gr.Textbox(label="AI", lines=3)],
-
 
 )
 
-web.launch(share=False)
+
+web_only_text = gr.Interface(
+
+	fn=preguntar,
+	
+	inputs=gr.Textbox(label="You", lines=3),
+	
+	outputs=["text"],
+	
+	title="VoiceBot",
+	# outputs=[gr.Textbox(label="AI", lines=3)],
+
+)
+
+# EJECUACION
+
+if __name__=="__main__":
+	
+	web.launch(share=False)
+	# web_only_text.launch()	
